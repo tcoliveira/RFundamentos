e1cefe6fac84fdb35ac8112b83150af00feab1f7
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2018-06-27 18:52:54 -0300

    Operações com listas

--- 08-Listas.R
+++ 08-Listas.R
@@ -57,3 +57,40 @@ lista2
 #Nomear diretamente
 lista2<-list(elementos1=3:5, elemento2=c(7.2,3.5))
 lista2
+
+#trabalhando com elementos especifico da lista
+
+lista1
+
+lista1$caracteres
+length(lista1$inteiros)
+lista1$inteiros
+
+length(lista1)
+
+
+mode(lista1$numericos)
+mode(lista1$caracteres)
+
+
+#combinando 2 listas
+
+lista3 <- c(lista1,lista2)
+lista3
+
+#transformando vetor em lista
+
+v =c(1:3)
+v
+
+l = as.list(v)
+l
+
+#unindo 2 elementos em uma lista
+
+mat = matrix(1:4, nr=2)
+mat
+vec = c(1:9)
+vec
+lst = list(mat,vec)
+lst
